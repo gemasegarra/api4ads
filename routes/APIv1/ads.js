51ac2a2856259e4dd61132c96c185d2186c1c3c1
gemasegarra <gema.segb@gmail.com>
2020-04-08 11:40:02 +0200

    get and post methods

--- routes/APIv1/ads.js
+++ routes/APIv1/ads.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const Ad = require('../../models/Ad');
 
+// GET /apiv1/ads
+// Returns list of ads
+
 router.get('/', async (req, res, next) => {
   try {
     const docs = await Ad.find()
@@ -11,5 +14,52 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 });
+
+
+// GET /apiv1/ads/:id
+// Returns ad with specific id
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const _id = req.params.id;
+    const ad = await Ad.findOne({_id });
+    if(!ad) {
+      const err = new Error ('Not found');
+      err.status = 404;
+      next(err);
+      return
+    }
+    res.json({result : ad});
+  } catch(err) {
+    next(err);
+  }
+});
+
+// POST /apiv1/ads
+// Creates new ad
+
+router.post('/', async (req, res, next) => {
+  try {
+ const adData = req.body;
+ const ad = new Ad(adData);
+ const savedAd = await ad.save();
+ res.status(201).json( {result: savedAd})
+} catch(err){
+  next(err)
+}
+})
+
+// DELETE /apiv1/ads/:id
+// Removes an ad
+/* router.delete('/:id', async (req, res, next) => {
+  try {
+    const _id = req.params.id;
+    await Ad.deleteOne({ _id });
+    res.json();
+  } catch(err) {
+    next(err);
+  }
+}) */
+
 
 module.exports = router;
