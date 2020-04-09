bf0e61139097fb65a3f66668f6678559e9650881
gemasegarra <gema.segb@gmail.com>
2020-04-09 19:34:22 +0200

    make routes work

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var router = express.Router();
-
-
+const Ad = require('../models/Ad');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Api4ads' });
+router.get('/', async function (req, res, next) {
+  const docs = await Ad.list();
+
+  res.render('index', {
+    title: 'API4ads', docs: docs
+  });
+})
+
+/* GET ad detail page */
+
+router.get('/:id', async function (req, res, next) {
+  try {
+  const ad = await Ad.findOne({ _id: req.params.id });
+  if(!ad) {
+    const err = new Error ('Not found');
+    err.status = 404;
+    next(err);
+    return
+  }
+  res.render('detail', ad);
+}
+catch(err) {
+  next(err);
+}
 });
 
 module.exports = router;
