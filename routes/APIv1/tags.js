d40578897dd615fb7c3f5ebe9a754b5b0cf586e6
gemasegarra <gema.segb@gmail.com>
2020-04-11 23:49:38 +0200

    fix bug of tag filtering

--- routes/APIv1/tags.js
+++ routes/APIv1/tags.js
@@ -2,18 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Ad = require('../../models/Ad');
 
-router.get('/', async (req, res, next) => {
-  let result = Ad.schema.path('tags').caster.validators[0].enumValues;
-  const docs = await Ad.list()
-  let usedTags = [];
-  docs.forEach(item => {
-    item.tags.forEach (tag=> {
-      if (usedTags.indexOf(tag) === -1){
-    usedTags.push(item.tags.toString())
-  }});  
-})
-console.log(usedTags)
-res.json({'Allowed tags': result, 'Tags in use:': usedTags})
-})
+router.get('/', async (req, res) => {
+  const result = Ad.schema.path('tags').caster.validators[0].enumValues;
+  const docs = await Ad.list();
+  const usedTags = [];
+  docs.forEach((item) => {
+    item.tags.forEach((tag) => {
+      if (!usedTags.includes(tag)) {
+        usedTags.push(tag);
+      }
+    });
+  });
+  res.json({ 'Allowed tags': result, 'Tags in use:': usedTags });
+});
 
 module.exports = router;
